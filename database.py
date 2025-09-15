a7db0179c461bbe80ff41e718877a22de3e8011d
OancaAdrian <oanca.adi@gmail.com>
2025-09-15 14:52:30 +0300

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-# Înlocuiește PAROLA_TA cu parola reală din Supabase → Database Settings → Database password
-DATABASE_URL = "postgresql://postgres:Adriangrila24$@postgres.nczdzhjhekabliwiryvc.supabase.co:6543/postgres?sslmode=require"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
